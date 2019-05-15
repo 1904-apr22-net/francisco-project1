ec5b9fc9c0eb6680ff3319724a98b9caa5853fde
Francisco Guerrero <guerrerof615@gmail.com>
2019-05-15 10:58:49 -0500

    deleted consoleapp

---
+++ HardwareStore/ConsoleApp/SecretConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HardwareStore.ProjectUI
+{
+    public static class SecretConfiguration
+    {
+        public static string ConnectionString = "Server=tcp:guerrero1904sql.database.windows.net,1433;Initial Catalog=HardwareStoreDb;Persist Security Info=False;User ID=francisco;Password=Password1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}
